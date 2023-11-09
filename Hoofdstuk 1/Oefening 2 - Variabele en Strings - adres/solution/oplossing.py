44bce77b541c48648eafe11c0a83eb98ca36f749
densissss <dennis.versyck@gmail.com>
2023-11-09 18:58:53 +0100

    oplossing

---
+++ Hoofdstuk 1/Oefening 2 - Variabele en Strings - adres/solution/oplossing.py
@@ -0,0 +1,5 @@
+# We maken de variabele adres aan en kennen de string 'Hugo Verrieststraat 22 - 8800 Roeselare' toe aan de variabele. 
+adres = 'Hugo Verrieststraat 22 - 8800 Roeselare'
+
+# We printen de inhoud van de variabele naar het scherm. 
+print(adres)
